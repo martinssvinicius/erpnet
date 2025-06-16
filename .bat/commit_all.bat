a61431d29f85414fbdb67919f594bd0ba07dc427
martinssvinicius <viniscmartins@gmail.com>
2025-06-16 16:37:40 -0300

    .

--- .bat/commit_all.bat
+++
@@ -1,4 +0,0 @@
-cd ../
-git add .
-git commit -m "."
-git push -u origin main
--- .bat/init.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd ../estrutura
-pg_ctl -D "C:\Program Files\PostgreSQL\16\data" start
-start "" /b sass scss/:css/ --watch
-php -S localhost:8000 router.php
